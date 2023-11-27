73d02ba958d0b1ec196d0be5de80acb5118bd49a
Jaturapat Dusadee <mr.jaturapat.d@gmail.com>
2023-11-27 12:35:35 +0700

    finish exercise hackhour

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,15 @@
 // Exercise #1: Food Order
 
 // Start coding here
+const foodOrder = {
+  orderName: "A0234",
+  address: "Bangkok",
+  paymetType: "Cash",
+  totalPrice: 4500,
+  restauantName: "MK",
+};
+
+foodOrder.totalPrice = 5000;
+foodOrder.paymetType = "Credit Card";
+
+console.log(foodOrder);
